fc38c7fdb53cd2b747b9848611ec45f03f20dd02
Gaurav1526 <gauravdhakad87573@gmail.com>
2025-06-01 02:15:19 +0530

    code

--- file_sharing_app/file_sharing_app/asgi.py
+++ file_sharing_app/file_sharing_app/asgi.py
@@ -13,4 +13,4 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'file_sharing_app.settings')
 
-app = get_asgi_application()
+application = get_asgi_application()
